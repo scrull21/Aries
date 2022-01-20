3caf1b170b9f506bee2963a92fc4e3fb6e7f9960
Toktorov <ktoktorov2005@gmail.com>
2022-01-21 00:08:06 +0600

    config django-ckeditor

--- kwst/urls.py
+++ kwst/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
