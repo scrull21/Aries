45ac50a5201fc9c9628c4bf513db3eec89319b38
Toktorov <ktoktorov2005@gmail.com>
2022-01-23 14:31:44 +0600

    add static and html template

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from apps.categories.models import Category
 
 # Create your views here.
+def index(request):
+    category = Category.objects.all()
+    context = {
+        'category' : category,
+    }
+    return render(request, 'index.html', context)
