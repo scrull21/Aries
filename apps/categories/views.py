8d087b84e58b4dfc8e66c766b11e491dd4e81d7c
Toktorov <ktoktorov2005@gmail.com>
2022-01-24 20:50:22 +0600

    output news in category

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from apps.categories.models import Category
+from apps.news.models import News
 
 # Create your views here.
 def category_index(request):
     category = Category.objects.all()
+    news = News.objects.all().order_by('-created',)[:10]
     context = {
         'categories' : category,
+        'news' : news,
     }
     return render(request, 'categories/index.html', context)
